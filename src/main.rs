3ef121156691740520c4e09e74d68ec150f6152b
Patrick Crumley <patrick.crumley@gmail.com>
2020-09-17 23:25:21 -0400

    some more error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use clap::{App, Arg};
 use rayon::prelude::*;
+use anyhow::{Context, Result, ensure};
 
 fn is_prime(num: u64) -> bool {
     !(2..num).any(|x| num % x == 0)
 }
 
 fn primes(start: u64, stop: u64) -> Vec<u64> {
+    // There 
+    // there are no primes less than 2 so need to
+    // start there.
+    let start = start.max(2);
     (start..=stop).into_par_iter()
         .filter_map(|n| {
             if is_prime(n) { Some(n) }
@@ -13,8 +18,9 @@ fn primes(start: u64, stop: u64) -> Vec<u64> {
         }).collect()
 }
 
-fn main() {
-    // The YAML file is found relative to the current file, similar to how modules are found
+fn main() -> Result<()> {
+    // Use the Clap.rs argument parser to capture user inputs. Does nice job of adding
+    // a help flag & throws returns an error if required args are missing.
     let matches = App::new("Parallel Primes")
                           .version("1.0")
                           .author("Patrick C. <patrick.crumley@gmail.com>")
@@ -28,8 +34,21 @@ fn main() {
                                 .required(true)
                                 .index(2))
                           .get_matches();
-    let start = matches.value_of("START").unwrap().parse::<u64>().unwrap();
-    let stop = matches.value_of("STOP").unwrap().parse::<u64>().unwrap();
+    // This unwrap does not cause a panic as "START" is a required arg, so Clap.rs throws error
+    // if it is missing.
+    let start = matches.value_of("START").unwrap();
 
+    // Convert the input into a u64 integer. Error handling by Anyhow crate.
+    let start = start.parse::<u64>()
+                .with_context(|| format!("Start must be an integer, you entered '{}'", start))?;
+
+    // handle parsing of stop same is start
+    let stop = matches.value_of("STOP").unwrap();
+    let stop = stop.parse::<u64>()
+                .with_context(|| format!("Stop must be an integer, you entered '{}'", stop))?;
+
+
+    // I believe the goal was to just print to stdout, so return the primes.
     println!("{:?}", primes(start, stop));
+    Ok(())
 }
